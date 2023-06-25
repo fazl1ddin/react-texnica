830018e59dc274d10a1e60f5836100bf9e54e91f
fazl1ddin <98404501+fazl1ddin@users.noreply.github.com>
2023-06-25 20:27:24 +0500

    Update Polzunok.jsx

--- src/components/Polzunok/Polzunok.jsx
+++ src/components/Polzunok/Polzunok.jsx
@@ -1,58 +1,40 @@
+import { useState } from "react"
+
 function Polzunok({step, min, max}){
     const [minv, setMin] = min
     const [maxv, setMax] = max
+    // const [minPos, setMinPos] = useState(0)
+    // const [maxPos, setMaxPos] = useState(0)
     let minPos = 0
     let maxPos = 0
-    let prevX = null
     let isDrag = false
-    const handler = (buttone) => {
+    const handler = (buttone, type) => {
         isDrag = true
-        prevX = buttone.clientX
-        maxPos = buttone.target.offsetParent.getBoundingClientRect().width
+        minPos = minPos || buttone.target.offsetParent.getBoundingClientRect().left - 14
+        maxPos = maxPos || buttone.target.offsetParent.getBoundingClientRect().width - 14
         let dotOB = buttone.clientX - buttone.target.getBoundingClientRect().left
         const handlerW = (mousee) => {
-            // console.log(maxPos);
-            // console.log(minPos);
             const xc = mousee.clientX
-            const bxc = buttone.clientX
-            if(isDrag){
-                minPos = (xc - buttone.target.offsetParent.getBoundingClientRect().left) - dotOB
-                let curX = mousee.clientX
-                // console.log(`xc : ${xc},`, `bxc : ${bxc},`, `minPos : ${minPos}`);
-                // if(curX > prevX){
-                //     minPos = xc - bxc
-                //     console.log('right');
-                //     buttone.target.style.transform = `translateX(${xc - bxc}px)`
-                // } else if(curX < prevX){
-                //     console.log('left');
-                //     buttone.target.style.transform = `translateX(${minPos + (xc - bxc)}px)`
-                // }
-                if(((buttone.target.offsetParent.getBoundingClientRect().left + dotOB) < mousee.clientX && minPos < maxPos)){
-                    buttone.target.style.transform = `translateX(${minPos}px)`
+            if (isDrag) {
+                if (type === 'min') {
+                    if(((buttone.target.offsetParent.getBoundingClientRect().left + dotOB) < mousee.clientX && (xc - buttone.target.offsetParent.getBoundingClientRect().left) - dotOB < (maxPos - 14))){
+                        minPos = (xc - buttone.target.offsetParent.getBoundingClientRect().left) - dotOB
+                        buttone.target.style.transform = `translateX(${minPos}px)`
+                    }
+                } else if (type === 'max') {
+                    if ((((buttone.target.offsetParent.getBoundingClientRect().left + buttone.target.offsetParent.getBoundingClientRect().width + dotOB - 14) > mousee.clientX) && ((minPos + 14 + dotOB) < ((xc - buttone.target.offsetParent.getBoundingClientRect().left) - dotOB)))) {
+                        maxPos = (xc - buttone.target.offsetParent.getBoundingClientRect().left) - dotOB
+                        buttone.target.style.right = `unset`
+                        buttone.target.style.transform = `translateX(${maxPos}px)`
+                    }
                 }
-                prevX = curX
-            }
-            // console.log('mouse xc  ' + xc, 'buttons xc  ' + bxc);
-            // if(((buttone.target.offsetParent.getBoundingClientRect().left + buttone.clientX - buttone.target.getBoundingClientRect().left) < mousee.clientX)){
-                // if(xc === bxc + buttone.clientX - buttone.target.getBoundingClientRect().left){
-                // } else if(xc > (bxc + (buttone.clientX - buttone.target.getBoundingClientRect().left))){
-                //     console.log('chapdan onga');
-                //     minPos = xc - bxc
-                //     buttone.target.style.transform = `translateX(${minPos}px)`
-                // } else if((bxc + (buttone.clientX - buttone.target.getBoundingClientRect().left)) > xc){
-                //     console.log('ondan chapga');
-                //     buttone.target.style.transform = `translateX(${bxc + minPos}px)`
-                // }
-            // }
-            // console.log(mousee); 
+            } 
         }
         window.addEventListener('mousemove', handlerW)
         window.addEventListener('mouseup', (mouseupe) => {
+            isDrag = false
             window.removeEventListener('mousemove', handlerW)
-            console.log(mouseupe);
         })
-        console.log(buttone);
-        console.log(buttone.target.offsetParent.getBoundingClientRect());
     }
 
     return <>
@@ -64,11 +46,11 @@ function Polzunok({step, min, max}){
                     <input value={maxv} type="text" onChange={e => setMax(Number(e.target.value))}/>
                 </div>
                 <div className="input-range">
-                    <div className="buttons" onMouseDown={handler} style={{top: 0, left: 0}}/>
+                    <div className="buttons" onMouseDown={(e) => handler(e, 'min')} style={{top: 0, left: 0}}/>
 
                     {/* <div className="bg" style={styleBg}></div> */}
 
-                    <div className="buttons" style={{top: 0, right: 0}}/>
+                    <div className="buttons" onMouseDown={(e) => handler(e, 'max')} style={{top: 0, right: 0}}/>
                 </div>
         </div>
     </>
